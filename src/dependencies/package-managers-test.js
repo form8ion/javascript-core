478c2d9a0c8c704db084227e57cf4cbe2d9749f9
Matt Travi <programmer@travi.org>
2021-01-05 00:09:58 -0600

    wip(package-managers): defined details for the available package-managers

--- src/dependencies/package-managers-test.js
+++ src/dependencies/package-managers-test.js
@@ -1,9 +1,38 @@
 import {assert} from 'chai';
-import managers from './package-managers';
+import {DEV_DEPENDENCY_TYPE, PROD_DEPENDENCY_TYPE} from './types';
+import {details} from './package-managers';
 
 suite('pacakge managers', () => {
   test('that the names match the cli names', () => {
-    assert.equal(managers.NPM, 'npm');
-    assert.equal(managers.YARN, 'yarn');
+    assert.equal({
+      NPM: 'npm',
+      YARN: 'yarn'
+    }.NPM, 'npm');
+    assert.equal({
+      NPM: 'npm',
+      YARN: 'yarn'
+    }.YARN, 'yarn');
+  });
+
+  test('that the installation details are correct for npm', () => {
+    const {installationCommand, installationFlags} = details[{
+      NPM: 'npm',
+      YARN: 'yarn'
+    }.NPM];
+
+    assert.equal(installationCommand, 'install');
+    assert.equal(installationFlags[DEV_DEPENDENCY_TYPE], 'save-dev');
+    assert.equal(installationFlags[PROD_DEPENDENCY_TYPE], 'save-prod');
+  });
+
+  test('that the installation details are correct for yarn', () => {
+    const {installationCommand, installationFlags} = details[{
+      NPM: 'npm',
+      YARN: 'yarn'
+    }.YARN];
+
+    assert.equal(installationCommand, 'add');
+    assert.equal(installationFlags[DEV_DEPENDENCY_TYPE], 'dev');
+    assert.equal(installationFlags[PROD_DEPENDENCY_TYPE], 'prod');
   });
 });
--- src/dependencies/package-managers.js
+++ src/dependencies/package-managers.js
@@ -1,6 +1,25 @@
+import {DEV_DEPENDENCY_TYPE, PROD_DEPENDENCY_TYPE} from './types';
+
 const packageManagers = {
   NPM: 'npm',
   YARN: 'yarn'
+};
+
+export const details = {
+  [packageManagers.NPM]: {
+    installationCommand: 'install',
+    installationFlags: {
+      [DEV_DEPENDENCY_TYPE]: `save-${DEV_DEPENDENCY_TYPE}`,
+      [PROD_DEPENDENCY_TYPE]: `save-${PROD_DEPENDENCY_TYPE}`
+    }
+  },
+  [packageManagers.YARN]: {
+    installationCommand: 'add',
+    installationFlags: {
+      [DEV_DEPENDENCY_TYPE]: DEV_DEPENDENCY_TYPE,
+      [PROD_DEPENDENCY_TYPE]: PROD_DEPENDENCY_TYPE
+    }
+  }
 };
 
 export default packageManagers;
