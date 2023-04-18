844c318ec6a18f24f1024d3d51fd48989150b306
Matt Travi <programmer@travi.org>
2023-04-18 18:10:31 -0500

    test(unit): added vitest as an additional runner and convered the node-versions tests
    to prepare to convert the remaining mocha tests to vitest

---
+++ src/node-versions.test.js
@@ -0,0 +1,45 @@
+import semver from 'semver';
+
+import {afterEach, describe, expect, it, vi} from 'vitest';
+import {when} from 'jest-when';
+import any from '@travi/any';
+
+import {determineLtsNodeMajorVersions, determineSupportedNodeMajorVersions} from './node-versions';
+
+vi.mock('semver');
+
+describe('node-versions', () => {
+  const providedRange = any.string();
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  describe('LTS major versions', () => {
+    it('should list the LTS major versions of node', async () => {
+      expect(determineLtsNodeMajorVersions()).toEqual([14, 16, 18, 20]);
+    });
+
+    it('should filter the list of active LTS versions by the provided semver range', async () => {
+      when(semver.satisfies).calledWith('12.0.0', providedRange).mockReturnValue(false);
+      when(semver.satisfies).calledWith('14.0.0', providedRange).mockReturnValue(true);
+      when(semver.satisfies).calledWith('16.0.0', providedRange).mockReturnValue(false);
+
+      expect(determineLtsNodeMajorVersions({withinRange: providedRange})).toEqual([14]);
+    });
+  });
+
+  describe('supported major versions', () => {
+    it('should list the major versions of node', async () => {
+      expect(determineSupportedNodeMajorVersions()).toEqual([14, 16, 18, 19, 20]);
+    });
+
+    it('should filter the list of supported versions by the provided semver range', async () => {
+      when(semver.satisfies).calledWith('14.0.0', providedRange).mockReturnValue(false);
+      when(semver.satisfies).calledWith('16.0.0', providedRange).mockReturnValue(true);
+      when(semver.satisfies).calledWith('18.0.0', providedRange).mockReturnValue(false);
+
+      expect(determineSupportedNodeMajorVersions({withinRange: providedRange})).toEqual([16]);
+    });
+  });
+});
