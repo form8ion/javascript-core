0c8587bf660c810368bd471cbc13cc216cea4148
Matt Travi <programmer@travi.org>
2023-07-06 15:20:18 -0500

    fix(options-validator): marked as deprecated since it has been lifted to the core package
    it will be removed with the next major version

--- src/options-validator.js
+++ src/options-validator.js
@@ -1,5 +1,9 @@
 import * as hoek from '@hapi/hoek';
 
+/**
+ * @deprecated this function has been lifted to the @form8ion/core package and will be removed in the next major
+ * version. please use that version instead
+ */
 export default function (schema, options) {
   const {error, value} = schema.validate(options);
 
