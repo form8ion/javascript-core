aa314c309ab4a66651f593c9aea43dc7e2898789
Matt Travi <programmer@travi.org>
2023-07-14 22:21:10 -0500

    feat(node-versions): removed v19 from the supported list
    since it has been EOL since June 1st: https://github.com/nodejs/Release#end-of-life-releases

--- src/node-versions.js
+++ src/node-versions.js
@@ -9,7 +9,7 @@ export function determineLtsNodeMajorVersions({withinRange} = {}) {
 }
 
 export function determineSupportedNodeMajorVersions({withinRange} = {}) {
-  return [14, 16, 18, 19, 20].filter(majorVersion => {
+  return [14, 16, 18, 20].filter(majorVersion => {
     if (!withinRange) return true;
 
     return semver.satisfies(`${majorVersion}.0.0`, withinRange);
